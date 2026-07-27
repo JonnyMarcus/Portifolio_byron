edd6e489430802b614f38ec7333958ffcdbe6c22
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-07-26 22:12:26 -0300

    feat: add estilização header

--- Capacitação_Traineer/semana_7/entrega/src/componets/header/index.tsx
+++ Capacitação_Traineer/semana_7/entrega/src/componets/header/index.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
 export default function Header() {
   return (
-    <header>
-      <Link href="">Receitas Deliciosas</Link>
-      <nav>
-        <Link href="/">Inicio</Link>
-        <Link href="/receitas">Receitas</Link>
-      </nav>
+    <header className=" w-full bg-white text-black py-4 ">
+      <div className=" flex justify-around container mx-auto">
+        <Link
+          className="text-xl font-bold houver: scale-x-105 transition-all"
+          href="/"
+        >
+          Receitas Deliciosas
+        </Link>
+        <nav className="flex gap-6">
+          <Link className=" houver: text-orange-500 transition-colors" href="/">
+            Inicio
+          </Link>
+          <Link
+            className=" houver: text-orange-500 transition-colors"
+            href="/receitas"
+          >
+            Receitas
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
