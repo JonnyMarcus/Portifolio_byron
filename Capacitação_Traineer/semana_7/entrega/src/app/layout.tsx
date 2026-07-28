dc28ca841f648b5774a5e6db3a893522217ebceb
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-07-27 21:40:12 -0300

    style: add logo e icon ao site e ao header

--- Capacitação_Traineer/semana_7/entrega/src/app/layout.tsx
+++ Capacitação_Traineer/semana_7/entrega/src/app/layout.tsx
@@ -3,7 +3,20 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../componets/header";
 import Footer from "../componets/footer";
+import { Poppins, DM_Serif_Display } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["600"], // SemiBold
+  variable: "--font-poppins",
+});
+
+const dmSerif = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: ["400"], // só existe peso 400 nessa fonte
+  style: ["normal", "italic"],
+  variable: "--font-dm-serif",
+});
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -20,7 +33,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={`${inter.variable} antialiased h-full `}>
+    <html
+      lang="pt-BR"
+      className={`${inter.variable} ${poppins.variable} ${dmSerif.variable} antialiased h-full`}
+    >
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
