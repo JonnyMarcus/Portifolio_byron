ddccd23626eaca1c62c8f7d15f598970450cd604
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-08-10 16:00:13 -0300

    feat: setup RHF e yup

--- Capacitação_Traineer/semana_9/entrega/src/componets/RecipeFormModal/page.tsx
+++ Capacitação_Traineer/semana_9/entrega/src/componets/RecipeFormModal/page.tsx
@@ -1,9 +1,15 @@
+import { FormState, useForm } from "react-hook-form";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "../../ui/dialog";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  RecipeFormData,
+  recipeSchema,
+} from "@/src/lib/formsValidationSchemas/recipeSchemas/recipeSchema";
 
 interface RecipeFormModalProps {
   isOpen: boolean;
@@ -14,6 +20,20 @@ export default function RecipeFormModal({
   isOpen,
   onClose,
 }: RecipeFormModalProps) {
+  const styleErrors = "text-sm text-red-500";
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RecipeFormData>({
+    resolver: yupResolver(recipeSchema),
+    mode: "onSubmit",
+  });
+
+  const onSubmit = (data: RecipeFormData) => {
+    (console.log(data), reset(), onClose());
+  };
   const inputStyle = "p-2 border border-zinc-200 rounded-md";
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -21,23 +41,49 @@ export default function RecipeFormModal({
         <DialogHeader>
           <DialogTitle>Nova receita</DialogTitle>
         </DialogHeader>
-        <form className="flex flex-col gap-4 w-full">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 w-full"
+        >
           <div className="grid grid-cols-2 gap-2">
             <div className="flex flex-col gap-1">
               {/* Titulo */}
               <label htmlFor="Titulo">Titulo</label>
-              <input className={inputStyle} type="text" id="title" />
+              <input
+                className={inputStyle}
+                type="text"
+                id="title"
+                {...register("title")}
+              />
+              {errors.title && (
+                <span className={styleErrors}>{errors.title.message}</span>
+              )}
             </div>
             <div className="flex flex-col gap-1">
               {/* Categoria */}
               <label htmlFor="Category">Categoria</label>
-              <input className={inputStyle} type="text" id="category" />
+              <input
+                className={inputStyle}
+                type="text"
+                id="category"
+                {...register("category")}
+              />
+              {errors.category && (
+                <span className={styleErrors}>{errors.category.message}</span>
+              )}
             </div>
           </div>
           {/* Descrição */}
           <div className="flex flex-col gap-1">
             <label htmlFor="description">Descrição</label>
-            <textarea className={inputStyle} id="description" />
+            <textarea
+              className={inputStyle}
+              id="description"
+              {...register("description")}
+            />
+            {errors.description && (
+              <span className={styleErrors}>{errors.description.message}</span>
+            )}
           </div>
           <div className="flex flex-col">
             {/* URl imagem */}
@@ -47,7 +93,11 @@ export default function RecipeFormModal({
               className={inputStyle}
               id="imageUrl"
               placeholder="/imagem.svg"
+              {...register("imageURL")}
             />
+            {errors.imageURL && (
+              <span className={styleErrors}>{errors.imageURL.message}</span>
+            )}
           </div>
           {/*  */}
           <div className="grid grid-cols-3 gap-2 items-center">
@@ -59,7 +109,11 @@ export default function RecipeFormModal({
                 type="text"
                 id="PrepTime"
                 placeholder="15 minutos"
+                {...register("prepTime")}
               />
+              {errors.prepTime && (
+                <span className={styleErrors}>{errors.prepTime.message}</span>
+              )}
             </div>
 
             <div className="flex flex-col gap-1">
@@ -70,7 +124,11 @@ export default function RecipeFormModal({
                 type="text"
                 id="cookTime"
                 placeholder="30 minutos"
+                {...register("cookTime")}
               />
+              {errors.cookTime && (
+                <span className={styleErrors}>{errors.cookTime.message}</span>
+              )}
             </div>
 
             <div className="flex flex-col gap-1">
@@ -81,7 +139,11 @@ export default function RecipeFormModal({
                 type="number"
                 id="servings"
                 defaultValue={1}
+                {...register("servings")}
               />
+              {errors.servings && (
+                <span className={styleErrors}>{errors.servings.message}</span>
+              )}
             </div>
           </div>
 
---
+++ Capacitação_Traineer/semana_9/entrega/src/lib/formsValidationSchemas/recipeSchemas/recipeSchema.ts
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+export const recipeSchema = yup.object().shape({
+  title: yup.string().required("O titulo é obrigatorio"),
+  category: yup.string().required("A categoria é obrigatorio"),
+  description: yup.string().required("A descrição é obrigatorio"),
+  imageURL: yup.string().required("A URL imagem é Obrigatoria"),
+  prepTime: yup.string().required("O tempo De preparo é obrigatorio"),
+  cookTime: yup.string().required("o tempo de cozimento é obrigatorio"),
+  servings: yup
+    .number()
+    .typeError("As porções devem ser um numero")
+    .positive("AS porções devem ser postivas")
+    .integer("As porções devem ser numeros interios")
+    .min(1, "Devem haver pelo menso 1 porção")
+    .required("A porções é obrigatoria"),
+});
+
+export type RecipeFormData = yup.InferType<typeof recipeSchema>;
